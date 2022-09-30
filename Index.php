74b0e1311521f130f065b25f58968430a4760fcf
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2022-09-29 21:47:00 -0300

    Airline Bin Laden

---
+++ Index.php
@@ -0,0 +1,31 @@
+<!-- Dispone de las apis de dos aerolíneas para gestionar la venta de pasajes de las mismas. Utilice inversión de dependencias para crear un sistema que gestione las ventas de pasajes de ambas aerolíneas permitiendo la utilización de reglas de negocios diferenciadas y particulares de la empresa. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include_once('components/navbar.php');
+    ?>
+    <main>
+
+        Cuerpo de la pagina
+
+    </main>
+    <?php
+    include_once('components/footer.php');
+    ?>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ components/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Bin Laden Airline</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    <a class="nav-link" href="#">Vuelos</a>
+                    <a class="nav-link" href="#">Login/Register</a>
+                </div>
+            </div>
+        </div>
+    </nav>
